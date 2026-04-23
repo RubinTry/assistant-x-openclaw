de113120979b9f163d3e6c26f4f672f17ede6156
Rubintry <13857769302@163.com>
2026-04-23 10:39:09 +0800

    补全未提交的models代码

---
+++ control_center/lib/models/log_entry.dart
@@ -0,0 +1,13 @@
+class LogEntry {
+  final DateTime timestamp;
+  final String message;
+
+  LogEntry({required this.timestamp, required this.message});
+
+  String get formattedTimestamp {
+    final hour = timestamp.hour.toString().padLeft(2, '0');
+    final minute = timestamp.minute.toString().padLeft(2, '0');
+    final second = timestamp.second.toString().padLeft(2, '0');
+    return '$hour:$minute:$second';
+  }
+}
